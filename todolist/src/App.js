8916405bf31d1d662d194e3913e3b08b71568d67
rachna rajak <113829584+rachnarajaka040@users.noreply.github.com>
2023-05-31 00:23:54 +0530

    add data type

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import './utils.css';
 
 function App() {
   return (
     <div className="App">
-      
+     
+      <header className="container">
+        <nav className="flex">
+          <div className="left flex items-center">
+           <img src="https://images.indianexpress.com/2021/01/myntra.png"/>
+           <ul class="flex items-center justify-center uppercase semibold">
+             <li>Men</li>
+             <li>Women</li>
+             <li>Kids</li>
+             <li>Home and Living</li>
+             <li>Beauty</li>
+             <li>Studio</li>
+           </ul>
+          </div>
+           <div className="right">
+
+           </div>
+        </nav>
+      </header>
     </div>
   );
 }
