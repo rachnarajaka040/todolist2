5688c882e2d5d256bd04c64e898d71d91ae59a4f
rachna rajak <113829584+rachnarajaka040@users.noreply.github.com>
2023-05-31 03:17:42 +0530

    add data type

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './utils.css';
-
+import pic from'./topimg.png';
 function App() {
   return (
     <div className="App">
@@ -20,7 +20,7 @@ function App() {
            </ul>
           </div>
            <div className="right flex items-center">
-            <input className=""/> 
+            <input className="input" type="text" placeholder="search product"/> 
             <div className="profile mx-2">Profile</div>
             <div className="wishlist mx-2">WishList</div>
             <div className="bag mx-2">Bag</div>
@@ -29,13 +29,7 @@ function App() {
         </nav>
       </header>
       <section className="container section1">
-      Imagine you own a big e-commerce website that sells watches in bulk. Your company is called Batch Watch — it’s your baby. One of the biggest assets the company has is the data it collects on vendors, its products, and customers.
-
-When you started the company, you didn’t have much cash to spend on an expensive database program, so you took the first inexpensive option that came your way. Unfortunately, this means your data is not well-modeled, and you’re not really sure how that initial data program’s technical skeleton is structured.
-
-After years of growth, you’ve now got cash to invest and want to implement a high quality database management system. However, you want to ensure the transfer is smooth and requires minimal downtime. You hire an IT consultant to execute the transfer.
-
-The IT consultant analyzes your data and its structure, and she decides on a new program that’s most efficient to store your data. The project outlines is as follows:
+      <img className="imgpic" src={pic}/>
       </section>
     </div>
   );
